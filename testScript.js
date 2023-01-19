a88089bbd02d051dae1b5b1cc2db0740cb8b3d96
Zefnar <zefnarmagus@gmail.com>
2023-01-19 15:56:23 -0600

    formatted better character sheet

--- testScript.js
+++ testScript.js
@@ -1,4 +1,4 @@
-document.getElementById('outputBox').style.display = 'none';
+// document.getElementById('outputBox').style.display = 'none';
 
 function generateNewCharSheet(newChar) {
     let userInFname = document.getElementById('fname').value
@@ -9,48 +9,54 @@ function generateNewCharSheet(newChar) {
 
     newChar = makeCharacter(userInFname, userInLevel, userInClass, userInRace, userInBg)
 
-    let sheetCharName = document.createElement('p')
-    sheetCharName.innerText = 'Name: ' + newChar._name;
-    document.body.appendChild(sheetCharName);
+    let sheetCharName = document.getElementById('name-box');
+    sheetCharName.innerHTML = 'Name: ' + userInFname;
 
-    let sheetHP = document.createElement('p')
-    sheetHP.innerText = 'HP: ' + newChar._hp;
-    document.body.appendChild(sheetHP);
+    let sheetHP = document.getElementById('hp-box');
+    sheetHP.innerHTML = 'HP: ' + newChar._hp + '/' + newChar._hp;
 
-    let sheetCharClass = document.createElement('p')
-    sheetCharClass.innerText = 'Class: ' + newChar._classMain.className;
-    document.body.appendChild(sheetCharClass);
+    let sheetCharClass = document.getElementById('class-box');
+    sheetCharClass.innerHTML = 'Class: ' + newChar._classMain.className;
     
-    let sheetCharLevel = document.createElement('p')
-    sheetCharLevel.innerText = 'Level: ' + newChar._chaLevel;
-    document.body.appendChild(sheetCharLevel);
+    let sheetCharLevel = document.getElementById('level-box');
+    sheetCharLevel.innerHTML = 'Level: ' + newChar._chaLevel;
 
-    let sheetProf = document.createElement('p')
-    sheetProf.innerText = 'Proficiency: +' + newChar._prof;
-    document.body.appendChild(sheetProf);
+    let sheetProf = document.getElementById('proficiency-box');
+    sheetProf.innerHTML = 'Prof: +' + newChar._prof;
     
-    let sheetCharRace = document.createElement('p')
-    sheetCharRace.innerText = 'Race: ' + newChar._race._raceName;
-    document.body.appendChild(sheetCharRace);
+    let sheetCharRace = document.getElementById('race-box');
+    sheetCharRace.innerHTML = newChar._race._raceName;
     
-    let sheetCharBg = document.createElement('p')
-    sheetCharBg.innerText = 'Background: ' + newChar._bg;
-    document.body.appendChild(sheetCharBg);
+    let sheetCharBg = document.getElementById('background-box');
+    sheetCharBg.innerHTML = 'Background: ' + newChar._bg;
     
-    let sheetStats = document.createElement('p')
-    sheetStats.innerText = 'Str: ' + newChar._stats[0] + ' Dex: ' + newChar._stats[1] + ' Con: ' + newChar._stats[2]  + ' Int: ' + newChar._stats[3] + ' Wis: ' + newChar._stats[4] + ' Cha: ' + newChar._stats[5]
-    document.body.appendChild(sheetStats);
+    let sheetStr = document.getElementById('str');
+    sheetStr.innerHTML = 'STR: ' + newChar._stats[0];
+    let sheetDex = document.getElementById('dex');
+    sheetDex.innerHTML = 'DEX: ' + newChar._stats[1];
+    let sheetCon = document.getElementById('con');
+    sheetCon.innerHTML = 'CON: ' + newChar._stats[2];
+    let sheetInt = document.getElementById('int');
+    sheetInt.innerHTML = 'INT: ' + newChar._stats[3];
+    let sheetWis = document.getElementById('wis');
+    sheetWis.innerHTML = 'WIS: ' + newChar._stats[4];
+    let sheetCha = document.getElementById('cha');
+    sheetCha.innerHTML = 'CHA: ' + newChar._stats[5];
 
-    let inventoryTitle = document.createElement('h3')
-    inventoryTitle.innerText = 'Inventory: '
-    let sheetInv = document.createElement('ul')
+    // let sheetStats = document.createElement('p')
+    // sheetStats.innerText = 'Str: ' + newChar._stats[0] + ' Dex: ' + newChar._stats[1] + ' Con: ' + newChar._stats[2]  + ' Int: ' + newChar._stats[3] + ' Wis: ' + newChar._stats[4] + ' Cha: ' + newChar._stats[5]
+    // document.body.appendChild(sheetStats);
+
+    // let inventoryTitle = document.createElement('h3')
+    // inventoryTitle.innerText = 'Inventory: '
+    let sheetInv = document.getElementById('backpack')
     for (i = 0; i < newChar.inv.length; i++) {
         let newInvItem = document.createElement('li')
         newInvItem.innerText = newChar.inv[i];
         sheetInv.appendChild(newInvItem);
     }
-    document.body.appendChild(inventoryTitle);
-    document.body.appendChild(sheetInv);
+    // document.body.appendChild(inventoryTitle);
+    // document.body.appendChild(sheetInv);
 
     document.getElementById('form-box').style.display = 'none';
     document.getElementById('main-title').style.display = 'none';
